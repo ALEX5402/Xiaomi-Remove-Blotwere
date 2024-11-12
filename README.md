b9b4bcf8f04e2187d8aeb4c4388636438263a833
Alex <alex5402private@gmail.com>
2024-11-12 21:33:49 +0530

    update Tue Nov 12 09:33:49 PM IST 2024

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # Xiaomi-Remove-Blotwere
-A bash script to remove blotwere from miui  and hypros 
+
+This script helps you uninstall pre-installed apps (bloatware) from MIUI devices. It can be executed using MT Manager with root privileges, in Termux with root, or via ADB shell.
+
+## How to Use
+
+### Using MT Manager (Root Required)
+1. Download and install MT Manager from [here](https://mt.junkfood.666.com).
+2. Grant root access to MT Manager.
+3. Copy the `uninstall_miui_bloatware.sh` script to your device.
+4. Open MT Manager and navigate to the script file.
+5. Tap on the script file and select `Execute`.
+
+### Using Termux (Root Required)
+1. Download and install Termux from the [Google Play Store](https://play.google.com/store/apps/details?id=com.termux) or [F-Droid](https://f-droid.org/packages/com.termux/).
+2. Open Termux and type the following command to grant root access:
+   ```bash
+   termux-sudo
+   ```
+3. Copy the `uninstall_miui_bloatware.sh` script to Termux's home directory.
+4. Navigate to the directory containing the script:
+   ```bash
+   cd /path/to/script
+   ```
+5. Make the script executable:
+   ```bash
+   chmod +x remove-blotwere.sh
+   ```
+6. Run the script:
+   ```bash
+   ./remove-blotwere.sh
+   ```
+
+### Using ADB Shell
+1. Enable Developer Options on your device.
+2. Enable USB Debugging in Developer Options.
+3. Connect your device to your computer via USB.
+4. Open a command prompt or terminal on your computer and navigate to the directory containing the script.
+5. Start the ADB shell:
+   ```bash
+   adb shell
+   ```
+6. Run the script:
+   ```bash
+   sh /path/to/uninstall_miui_bloatware.sh
+   ```
+
+## Command Reference
+
+### Uninstall Command
+To uninstall an app, use the following command:
+```bash
+pm uninstall -k --user 0 [package_name]
+```
+
+### Reinstall Command
+To reinstall an uninstalled app, use the following command:
+```bash
+cmd package install-existing [package_name]
+```
+
+### Disable Command
+To disable an app, use the following command:
+```bash
+pm disable-user --user 0 [package_name]
+```
+
+### Enable Command
+To enable a disabled app, use the following command:
+```bash
+pm enable [package_name]
+```
+
+## Disclaimer
+Be cautious while uninstalling system apps, as it might affect the functionality of your device. Always ensure you have backups of important data before proceeding with uninstallation.
+```
+This README.md file provides clear instructions on how to use the script across different environments. Let me know if you need any further modifications or additional information! 😊
+```
---
+++ remove-blotwere.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# some packagenames might be old and diffrent
+pm uninstall -k --user 0 com.miui.analytics          # Analytics
+pm uninstall -k --user 0 com.mi.globalminusscreen           # App Vault
+pm uninstall -k --user 0 com.miui.backup             # Backup
+pm uninstall -k --user 0 com.miui.browser            # Browser
+
+pm uninstall -k --user 0 com.miui.digitalwellbeing   # Digital Wellbeing
+pm uninstall -k --user 0 com.miui.downloads          # Downloads
+pm uninstall -k --user 0 com.miui.fmradio            # FM Radio
+pm uninstall -k --user 0 com.facebook.katana         # Facebook
+pm uninstall -k --user 0 com.miui.feedback           # Feedback
+pm uninstall -k --user 0 com.miui.games              # Games
+pm uninstall -k --user 0 com.xiaomi.mipicks            # GetApps (Only for Global ROM)
+pm uninstall -k --user 0 com.miui.hybridaccessory    # Hybrid Accessory
+pm uninstall -k --user 0 com.miui.msa                # MSA
+pm uninstall -k --user 0 com.miui.marketfeedbackagent # Market Feedback Agent
+pm uninstall -k --user 0 com.miui.mail               # Mail
+pm uninstall -k --user 0 com.miui.cloud              # Mi Cloud
+pm uninstall -k --user 0 com.miui.credit             # Mi Credit
+pm uninstall -k --user 0 com.google.android.apps.messaging
+
+pm uninstall -k --user 0 com.miui.mover              # Mi Mover
+pm uninstall -k --user 0 com.miui.pay                # Mi Pay
+
+pm uninstall -k --user 0 com.miui.recycle            # Mi Recycle
+pm uninstall -k --user 0 com.miui.connectservice     # MiConnectService
+pm uninstall -k --user 0 com.miui.playclient         # MiPlayClient
+pm uninstall -k --user 0 com.miui.wallpapercarousel  # Mi Wallpaper Carousel
+pm uninstall -k --user 0 com.miui.daemon             # MiuiDaemon
+pm uninstall -k --user 0 com.miui.music              # Music
+pm uninstall -k --user 0 com.miui.nextpay            # NextPay
+pm uninstall -k --user 0 com.miui.notes              # Notes
+pm uninstall -k --user 0 com.miui.pai                # PAI
+pm uninstall -k --user 0 com.miui.partnerbookmarks   # P
+pm uninstall -k --user 0 com.miui.quickapps          # Quick Apps
+pm uninstall -k --user 0 com.miui.quickball   
+pm uninstall -k --user 0 com.miui.search             
+pm uninstall -k --user 0 com.miui.smsextra           # SMS Extra
+pm uninstall -k --user 0 com.miui.scanner            # Scanner
+pm uninstall -k --user 0 com.miui.services           # Services & Feedback
+pm uninstall -k --user 0 com.miui.soterservice       # Soter Service
+pm uninstall -k --user 0 com.miui.uniservice         # UniPlay Service
+pm uninstall -k --user 0 com.miui.yellowpages        # Yellow Pages
+
+
+# gapps and some other stuff
+pm uninstall -k --user 0 com.google.android.apps.subscriptions.red
+pm uninstall -k --user 0 com.google.android.apps.healthdata
+pm uninstall -k --user 0 com.google.android.apps.setupwizard.searchselector
+pm uninstall -k --user 0 com.miui.player
+pm uninstall -k --user 0 com.miui.global.packageinstaller
+pm uninstall -k --user 0 com.milink.service
+pm uninstall -k --user 0 com.xiaomi.payment
+pm uninstall -k --user 0 com.xiaomi.mi_connect_service
+pm uninstall -k --user 0 com.miui.msa.global
+pm uninstall -k --user 0 com.mi.healthglobal
+pm uninstall -k --user 0 com.google.android.apps.safetyhub
+pm uninstall -k --user 0 com.android.nfc
+pm uninstall -k --user 0 com.google.android.apps.tachyon
+pm uninstall -k --user 0 com.facebook.services
+pm uninstall -k --user 0 com.miui.misound
+pm uninstall -k --user 0 com.miui.bugreport
+pm uninstall -k --user 0 com.google.android.apps.wellbeing
+pm uninstall -k --user 0 com.miui.cloudbackup
+pm uninstall -k --user 0 com.google.android.videos
+pm uninstall -k --user 0 com.miui.cloudservice
+pm uninstall -k --user 0 com.google.android.apps.turbo
+pm uninstall -k --user 0 com.miui.miservice
+pm uninstall -k --user 0 com.miui.micloudsync
+
+echo "Uninstallation complete."
+
